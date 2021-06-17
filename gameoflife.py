0168f5d50afb6fef12c81efa1c878e230ad2ea10
Serpentina747 <sergi.petrache@gmail.com>
2021-06-17 16:16:07 +0200

    Added some functions

--- gameoflife.py
+++ gameoflife.py
@@ -12,7 +12,7 @@ def createPolygon(x, y):
 
 def initializeGameState():
     running = True
-    gameState = np.zeros((width_cells_number, height_cells_number))
+    gameState = np.zeros((width_cells_number, height_cells_number)) # We set all the gameState matrix to 0s
     screen = pygame.display.set_mode((height, width)) # Initialize screen
     pygame.display.set_caption("Game of Life")        # Put some cool title
     screen.fill(background)
@@ -23,12 +23,15 @@ def initializeGameState():
             if event.type == pygame.QUIT:           # If quit event is activated, stop the program
                 running = False
 
-            mouseClick = pygame.mouse.get_pressed()
+            mouseClick = pygame.mouse.get_pressed() # If some click of the mouse is pressed, we interact with the gameState matrix
             if sum(mouseClick) > 0:
                 posX, posY = pygame.mouse.get_pos()
                 celX = int(np.floor(posX / cell_width))
                 celY = int(np.floor(posY / cell_height))
-                gameState[celY, celX] = 1
+                if mouseClick[0] == 1:              # Create alive cells if left click is pressed
+                    gameState[celY, celX] = 1
+                elif mouseClick[2] == 1:            # Create death cells if right click is pressed
+                    gameState[celY, celX] = 0
 
             if event.type == pygame.KEYDOWN:
                 running = False
@@ -68,10 +71,59 @@ def evaluteCell(gameState, x, y):
     else:
         return [(255, 255, 255), 0]
 
-    
-pygame.init() # Initialize all pygame modules
 
-running = True
+def main():
+
+    running = True
+    pygame.init() # Initialize all pygame modules
+
+    # Number 0 for dead cells, 1 for alive.
+    gameState = initializeGameState()
+
+    screen = pygame.display.set_mode((height, width)) # Initialize screen
+    pygame.display.set_caption("Game of Life")        # Put some cool title
+    screen.fill(background)
+
+
+    while running:
+
+        newGameState = np.copy(gameState)
+        screen.fill(background)
+        time.sleep(0.01)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:           # If quit event is activated, stop the program
+                running = False
+            
+        for x in range(0, height_cells_number):             # Iterate each row
+            for y in range(0, width_cells_number):          # Iterate each column in a row
+                
+                # Compute alive neighbours
+                neighbours = getAliveNeighbours(gameState, x, y)
+
+                # First rule: a death cell that has exactly 3 alive neighbours becomes alive
+                if gameState[y, x] == 0 and neighbours == 3:
+                    newGameState[y, x] = 1
+
+                # Second rule: an alive cell that has les then 2 or more then 3 neighbours alive, dies
+                elif(gameState[y, x] == 1 and neighbours < 2 or neighbours > 3):
+                    newGameState[y, x] = 0
+
+                # We design the cell
+                polygon = createPolygon(x,y)
+                
+                # Evaluate if the cell is death or alive to return the drawn polygon type
+                newValues = evaluteCell(gameState, x, y)
+                # Put it on the screen
+                pygame.draw.polygon(screen, newValues[0], polygon, newValues[1])
+
+        gameState = np.copy(newGameState)
+        # Display it
+        pygame.display.flip()
+
+    pygame.display.quit()
+    pygame.quit()
+    exit()
+
 width_cells_number  = 100 # Number of cells in the total height of the screen
 height_cells_number = 100 # Number of cells in the total width of the screen
 
@@ -85,51 +137,5 @@ polygon_colour = (0, 0, 0, 1)       # Black color for the cells margins
 cell_height = height / height_cells_number      # Cells height
 cell_width = width / width_cells_number         # Cells width
 
-# Number 0 for dead cells, 1 for alive.
-gameState = initializeGameState()
-
-print(gameState)
-
-screen = pygame.display.set_mode((height, width)) # Initialize screen
-pygame.display.set_caption("Game of Life")        # Put some cool title
-screen.fill(background)
-
-
-while running:
-
-    newGameState = np.copy(gameState)
-    screen.fill(background)
-    time.sleep(0.01)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:           # If quit event is activated, stop the program
-            running = False
-            
-    for x in range(0, height_cells_number):             # Iterate each row
-        for y in range(0, width_cells_number):          # Iterate each column in a row
-                
-            # Compute alive neighbours
-            neighbours = getAliveNeighbours(gameState, x, y)
-
-            # First rule: a death cell that has exactly 3 alive neighbours becomes alive
-            if gameState[y, x] == 0 and neighbours == 3:
-                newGameState[y, x] = 1
-
-            # Second rule: an alive cell that has les then 2 or more then 3 neighbours alive, dies
-            elif(gameState[y, x] == 1 and neighbours < 2 or neighbours > 3):
-                newGameState[y, x] = 0
-
-            # We design the cell
-            polygon = createPolygon(x,y)
-                
-            # Evaluate if the cell is death or alive to return the drawn polygon type
-            newValues = evaluteCell(gameState, x, y)
-            # Put it on the screen
-            pygame.draw.polygon(screen, newValues[0], polygon, newValues[1])
-
-    gameState = np.copy(newGameState)
-    # Display it
-    pygame.display.flip()
-
-pygame.display.quit()
-pygame.quit()
-exit()
+if __name__ == "__main__":
+    main()
